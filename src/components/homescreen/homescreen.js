20c97400f8623f7216961b78ab37f86a0773436c
wiliyam <wiliyambhadani@gmail.com>
2019-12-26 01:37:36 +0530

    fix : call server stream

--- src/components/homescreen/homescreen.js
+++ src/components/homescreen/homescreen.js
@@ -8,38 +8,77 @@ import { chatappServiceClient } from "../../generated/chatappService_grpc_web_pb
 import { req, res } from "../../generated/chatappService_pb";
 
 const client = new chatappServiceClient("https://api.4star.ga", null, null);
-
+const request = new req();
+let metadata = {};
 let call;
 const HomePage = () => {
-  let id = moment().unix();
+  // let id = moment().unix();
   const [state, setState] = useState({
-    name: ""
+    name: "",
+    token: "",
+    newMsg: []
   });
 
-  const onConn = async () => {
-    const metadata = { id: id };
-    const request = new req();
-
-    let res = {
-      msg: "hello from client..",
-      // name,
-      handShack: 0,
-      id
+  const subscribe = async token => {
+    let reqData = {
+      msg: "hello from client..looking for subscribe data...",
+      name: state.name,
+      handShack: 1
+    };
+    metadata = {
+      token: token
     };
 
+    console.log("metadata", metadata);
+    request.setMessage(JSON.stringify(reqData));
+    let callSub = client.subscribe(request, metadata, (err, response) => {});
+    callSub.on("data", res => {
+      let response = JSON.parse(res.getMessage());
+
+      setState({
+        newMsg: response.newMsg
+      });
+      console.log("response", response);
+    });
+    setTimeout(() => {
+      console.log("timeout....");
+      callSub.on("cencel");
+    }, 2000);
+    callSub.on("end", a => {
+      console.log("server conn left...");
+    });
+  };
+
+  const onConn = async () => {
+    let reqData = {
+      msg: "hello from client..looking for token...",
+      name: state.name,
+      handShack: 0
+      // id
+    };
+    console.log("reqData", reqData);
     // Cookie.set("username", state.name);
-    request.setMessage(JSON.stringify(res));
-    call = client.subscribe(request, metadata, (err, response) => {
-      console.log("error", err);
-      console.log("response", response.getMessage());
-      if (response == null) {
-        console.log(err);
-      } else {
-        console.log("response", response);
+    request.setMessage(JSON.stringify(reqData));
+    call = client.newUsr(request, metadata, (err, response) => {
+      response = JSON.parse(response.getMessage());
+      console.log("--", response);
+      if (response.code == 409) {
+        console.log("409...", response.token);
+        if (typeof response.token == undefined) {
+          console.log("token not found...");
+        }
+        setState({
+          token: response.token
+        });
+      }
+      setState({
+        token: response.token
+      });
+
+      if (state.token != "") {
+        subscribe(response.token);
       }
     });
-
-    call.on("data", res => console.log("--", res));
   };
 
   const onNameChange = e => {
@@ -65,6 +104,12 @@ const HomePage = () => {
           <button type="button" class="btn btn-primary" onClick={onConn}>
             Submit
           </button>
+          <ul>
+            {state.newMsg &&
+              state.newMsg.map(item => {
+                return <div>{item}</div>;
+              })}
+          </ul>
         </form>
       </div>
 
